8d147fd7ab21f0ada28f130ee1078298c8e39b4e
rawlin <rawlincrasto@gmail.com>
2020-10-23 23:35:34 +0530

    groupie recyclerview

--- app/src/main/java/com/example/chatapp/NewMessageActivity.kt
+++ app/src/main/java/com/example/chatapp/NewMessageActivity.kt
@@ -2,11 +2,58 @@ package com.example.chatapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import com.xwray.groupie.GroupAdapter
+import com.xwray.groupie.Item
+import com.xwray.groupie.ViewHolder
+import kotlinx.android.synthetic.main.activity_new_message.*
+import kotlinx.android.synthetic.main.user_row_new_message.view.*
+import timber.log.Timber
 
 class NewMessageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_new_message)
 
+        supportActionBar?.title="Select User"
+
+        val adapter=GroupAdapter<ViewHolder>()
+
+        recyclerview_newmessage.adapter=adapter
+        fetchUsers()
+
+    }
+
+    private fun fetchUsers() {
+        val ref=FirebaseDatabase.getInstance().getReference("/users")
+        ref.addValueEventListener(object:ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot) {
+                val adapter=GroupAdapter<ViewHolder>()
+                snapshot.children.forEach{
+                    Timber.d(it.toString())
+                    val user=it.getValue(User::class.java)
+                    if(user!=null){
+                       adapter.add(UserItem(user))
+                    }
+                }
+                recyclerview_newmessage.adapter=adapter
+            }
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+        })
+    }
+}
+
+class UserItem(val user:User): Item<ViewHolder>(){
+    override fun bind(viewHolder: ViewHolder, position: Int) {
+        viewHolder.itemView.username_textview_new_message.text=user.username
+    }
+
+    override fun getLayout(): Int {
+        return R.layout.user_row_new_message
     }
 }
